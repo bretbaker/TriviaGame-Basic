07892f31a5f9f1ce3cd560040ed867d4697e44eb
Bret Baker <bretbaker808@gmail.com>
2019-07-15 14:23:05 -0700

    basic timer working, now need to make radio input multiple choice

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,32 @@
 // hide timer and questions onload
+
 $(window).on('load', function() {
     $("#timer-questions-container").hide();
 });
+
+
+// create timer
+
+let timeLeft = 120;
+
+let timeInterval;
+
+const timer = () => {
+    clearInterval(timeInterval);
+    timeInterval = setInterval(countDown, 1000);
+}
+
+const countDown = () => {
+    timeLeft--;
+    $("#game-timer").text("Time Remaining: " + timeLeft + " Seconds");
+}
+
 
 // ready events
 $(document).ready(function() {
     $("#start-button").on('click', function() {
         $("#start-button").hide();
         $("#timer-questions-container").show();
+        timer();
     });
 });
